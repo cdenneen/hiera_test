b418930b6b9222580cd140d007080a33547e7d9f
Chris Denneen <cdenneen@gmail.com>
2014-08-08 14:14:56 -0400

    updated module manifest with fix

--- puppet/modules/mymodule/manifests/init.pp
+++ puppet/modules/mymodule/manifests/init.pp
@@ -2,10 +2,12 @@
 
 class mymodule (
   $template = undef,
-  $options  = undef,
-  #$options  = hiera('$options', undef),
-  #$options  = hiera_hash('$options', undef),
+  #$options  = undef,
+  $my_options  = hiera_hash('mymodule::options', undef)
 ){
+  $options = $my_options
+  # or get rid of $my_options
+  # $options  = hiera_hash('mymodule::options', undef)
 
   file { "mymodule_config":
     path    => '/tmp/mymodule.conf',
